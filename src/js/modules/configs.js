8202272dcf6bff0aeda3367f2b1454d5e3483444
vorvarinoo <n.styrman@yandex.ru>
2024-10-28 02:06:16 +0300

    finish

--- src/js/modules/configs.js
+++ src/js/modules/configs.js
@@ -4,6 +4,30 @@ const sliderConfig = {
     spaceBetween: 30,
     watchSlidesProgress: true,
   },
+  houseSlider: {
+    watchSlidesProgress: true,
+    slidesPerView: 1,
+    allowTouchMove: false,
+    navigation: {
+      nextEl: ".slider-button-next",
+      prevEl: ".slider-button-prev",
+    },
+  },
+  houseThumbsSlider: {
+    watchSlidesProgress: true,
+    slidesPerView: 3,
+    direction: "vertical",
+    spaceBetween: 16,
+    freeMode: true,
+  },
+  houseImgSlider: {
+    watchSlidesProgress: true,
+    slidesPerView: 1,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  },
 };
 
 const smoothScrollConfig = {
@@ -21,31 +45,31 @@ const modalConfig = {
 
 const validateConfig = {
   justValidate: {
-    errorFieldCssClass: 'is-invalid',
+    errorFieldCssClass: "is-invalid",
     errorLabelStyle: {
-      color: 'var(--error)',
-      marginTop: '6px',
-      fontSize: '12px',
-      textAlign: 'left',
+      color: "var(--error)",
+      marginTop: "6px",
+      fontSize: "12px",
+      textAlign: "left",
     },
-    errorFormClass: 'shaked',
+    errorFormClass: "shaked",
     errorTimeout: 1500,
   },
   errorTimeout: 1500,
   mask: {
-    bodyMask: ' (___) ___ __ __',
-  }
+    bodyMask: " (___) ___ __ __",
+  },
 };
 
 const observerConfig = {
   scrollTop: {
-    rootMargin: '600px',
+    rootMargin: "600px",
     threshold: 1,
   },
 };
 
 const requestsConfig = {
-  handlerURL: 'https://jsonplaceholder.typicode.com/posts',
+  handlerURL: "https://jsonplaceholder.typicode.com/posts",
 };
 
 export {
--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,15 +1,29 @@
-import {
-  initSlider
-} from './utils.js';
+import { initSlider } from "./utils.js";
 
-import {
-  sliderConfig
-} from './configs.js';
+import { sliderConfig } from "./configs.js";
 
 const initSliders = () => {
-  initSlider( '.main-first', sliderConfig );
+  initSlider(".main-first", sliderConfig);
+  initSlider(".house-slider", sliderConfig.houseSlider);
+
+  // Получаем все парные элементы house-thumbs-slider и house-img-slider
+  const thumbsSliders = document.querySelectorAll(".house-thumbs-slider");
+  const imgSliders = document.querySelectorAll(".house-img-slider");
+
+  thumbsSliders.forEach((thumbsSlider, index) => {
+    // Инициализируем thumbsSlider для текущего imgSlider
+    const houseThumbsSwiper = initSlider(
+      thumbsSlider,
+      sliderConfig.houseThumbsSlider
+    );
+
+    // Настраиваем и инициализируем houseImgSlider с текущим houseThumbsSlider
+    const houseImgSliderConfig = { ...sliderConfig.houseImgSlider };
+    houseImgSliderConfig.thumbs = { swiper: houseThumbsSwiper };
+
+    // Инициализируем houseImgSlider с созданной конфигурацией
+    initSlider(imgSliders[index], houseImgSliderConfig);
+  });
 };
 
-export {
-  initSliders,
-};
+export { initSliders };
